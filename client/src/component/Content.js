3bc544643f3bcc5fc29e78ebcd4b2300501a02cc
Seungmin Kim <k0102575@gmail.com>
2020-08-02 23:38:36 +0900

    button & file upload box publishing

--- client/src/component/Content.js
+++ client/src/component/Content.js
@@ -3,8 +3,15 @@ import React from 'react';
 // Style
 import styled from 'styled-components';
 
+// Component
+import Converter from './Converter';
+
 const Content = () => {
-  return <ContentContainer>Content</ContentContainer>;
+  return (
+    <ContentContainer>
+      <Converter />
+    </ContentContainer>
+  );
 };
 
 export default Content;
@@ -12,4 +19,8 @@ export default Content;
 const ContentContainer = styled.div`
   flex: 1 1 auto;
   background-color: ${(props) => props.theme.ContentBackGroundColor};
+  padding: 0 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
